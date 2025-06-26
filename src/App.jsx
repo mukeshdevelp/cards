39306a24ef81efe02daa60e2f658f15bb176756c
mukesh <msmukeshkumarsharma95@gmail.com>
2025-06-26 12:24:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Heading from './components/Heading'
+
+function App() {
+  
+
+  return (
+    <>
+
+    <Heading />
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Heading.jsx
@@ -0,0 +1,9 @@
+const Heading = () => {
+    return (
+        <>
+            <h1 className="">Thought-Provoking Stories</h1>
+        </>
+    )
+} 
+
+export default Heading;
