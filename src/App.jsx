a2f3c097373631635d0ad465d816389deba362bc
mukesh <msmukeshkumarsharma95@gmail.com>
2025-06-27 10:14:14 +0530

    card project complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,94 +3,14 @@ import './App.css'
 import Card from './components/Card'
 import Heading from './components/Heading'
 import image from './assets/image.jpeg'
+import cardObject from './utils/CardObject'
+import path from './utils/Path';
 function App() {
  const path = image
- const card = [{
-        id: 1,
-
-        imgSrc: image,
-        companyName: "Vougue India",
-        blogTitle: "Of Course I Like Match, I Used to Eat Chalk",
-        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, aliquid ad, quo doloribus placeat, voluptatem nulla facere at accusamus illum sequi in numquam sed doloremque voluptatum! Blanditiis dicta nam hic eos. Labore culpa ea temporibus necessitatibus blanditiis! Quidem quis voluptatem est earum deserunt esse, modi, laborum corporis hic cum, eos non corrupti soluta consectetur fugit facere"
-
- },
- {
-        id: 5,
-
-        imgSrc: image,
-        companyName: "Vougue India",
-        blogTitle: "Of Course I Like Match, I Used to Eat Chalk",
-        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, aliquid ad, quo doloribus placeat, voluptatem nulla facere at accusamus illum sequi in numquam sed doloremque voluptatum! Blanditiis dicta nam hic eos. Labore culpa ea temporibus necessitatibus blanditiis! Quidem quis voluptatem est earum deserunt esse, modi, laborum corporis hic cum, eos non corrupti soluta consectetur fugit facere"
-
-    },
-  ]
-  let cardObject = [
 
 
-    {
-        id: 1,
-
-        imgSrc: image,
-        companyName: "Vougue India",
-        blogTitle: "Of Course I Like Match, I Used to Eat Chalk",
-        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, aliquid ad, quo doloribus placeat, voluptatem nulla facere at accusamus illum sequi in numquam sed doloremque voluptatum! Blanditiis dicta nam hic eos. Labore culpa ea temporibus necessitatibus blanditiis! Quidem quis voluptatem est earum deserunt esse, modi, laborum corporis hic cum, eos non corrupti soluta consectetur fugit facere"
-
-    },
-
-    {
-        id: 2,
-
-        imgSrc: image,
-        companyName: "Vougue India",
-        blogTitle: "Of Course I Like Match, I Used to Eat Chalk",
-        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, aliquid ad, quo doloribus placeat, voluptatem nulla facere at accusamus illum sequi in numquam sed doloremque voluptatum! Blanditiis dicta nam hic eos. Labore culpa ea temporibus necessitatibus blanditiis! Quidem quis voluptatem est earum deserunt esse, modi, laborum corporis hic cum, eos non corrupti soluta consectetur fugit facere"
-
-    },
-
-    {
-        id: 3,
-
-        imgSrc: image,
-        companyName: "Vougue India",
-        blogTitle: "Of Course I Like Match, I Used to Eat Chalk",
-        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, aliquid ad, quo doloribus placeat, voluptatem nulla facere at accusamus illum sequi in numquam sed doloremque voluptatum! Blanditiis dicta nam hic eos. Labore culpa ea temporibus necessitatibus blanditiis! Quidem quis voluptatem est earum deserunt esse, modi, laborum corporis hic cum, eos non corrupti soluta consectetur fugit facere"
-
-    },
 
 
-    {
-        id: 4,
-
-        imgSrc: image,
-        companyName: "Vougue India",
-        blogTitle: "Of Course I Like Match, I Used to Eat Chalk",
-        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, aliquid ad, quo doloribus placeat, voluptatem nulla facere at accusamus illum sequi in numquam sed doloremque voluptatum! Blanditiis dicta nam hic eos. Labore culpa ea temporibus necessitatibus blanditiis! Quidem quis voluptatem est earum deserunt esse, modi, laborum corporis hic cum, eos non corrupti soluta consectetur fugit facere"
-
-    },
-
-
-    {
-        id: 5,
-
-        imgSrc: image,
-        companyName: "Vougue India",
-        blogTitle: "Of Course I Like Match, I Used to Eat Chalk",
-        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, aliquid ad, quo doloribus placeat, voluptatem nulla facere at accusamus illum sequi in numquam sed doloremque voluptatum! Blanditiis dicta nam hic eos. Labore culpa ea temporibus necessitatibus blanditiis! Quidem quis voluptatem est earum deserunt esse, modi, laborum corporis hic cum, eos non corrupti soluta consectetur fugit facere"
-
-    },
-
-    {
-        id: 6,
-
-        imgSrc: image,
-        companyName: "Vougue India",
-        blogTitle: "Of Course I Like Match, I Used to Eat Chalk",
-        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, aliquid ad, quo doloribus placeat, voluptatem nulla facere at accusamus illum sequi in numquam sed doloremque voluptatum! Blanditiis dicta nam hic eos. Labore culpa ea temporibus necessitatibus blanditiis! Quidem quis voluptatem est earum deserunt esse, modi, laborum corporis hic cum, eos non corrupti soluta consectetur fugit facere"
-
-    },
-]
-
-//const [initialItems, setItems] = useState(card);
 
 
   
--- src/utils/CardObject.js
+++ src/utils/CardObject.js
@@ -1,10 +1,11 @@
-let cardObjs = [
+import path from "./Path";
+let cardObject = [
 
 
     {
         id: 1,
 
-        imgSrc: "https://s3.us-east-1.amazonaws.com/pocket-curatedcorpusapi-prod-images/bf6099ed-57a0-41fe-ac1c-0ca058fa0467.jpeg",
+        imgSrc: path,
         companyName: "Vougue India",
         blogTitle: "Of Course I Like Match, I Used to Eat Chalk",
         content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, aliquid ad, quo doloribus placeat, voluptatem nulla facere at accusamus illum sequi in numquam sed doloremque voluptatum! Blanditiis dicta nam hic eos. Labore culpa ea temporibus necessitatibus blanditiis! Quidem quis voluptatem est earum deserunt esse, modi, laborum corporis hic cum, eos non corrupti soluta consectetur fugit facere"
